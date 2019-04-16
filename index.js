fec5df5346b10a068ce905621f43b1daa3a8ec85
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-16 01:27:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(arrOfMusicians, arrOfInstruments){
+  let newArr = []
+  for(let i = 0; i < arrOfMusicians.length; i++){
+    newArr.push(arrOfMusicians[i] + " plays " + arrOfInstruments[i])
+  }
+  return newArr
+}
+
+function johnLennonFacts(arrOfFacts){
+let newArr = []
+let i = 0
+  while(i < arrOfFacts.length){
+    newArr.push(arrOfFacts[i] + "!!!")
+    i++
+  }
+  return newArr
+}
+
+function iLoveTheBeatles(num){
+  let variable = []
+  do {
+    variable.push("I love the Beatles!")
+    num++
+  } while (num < 15){
+  }
+  return variable
+}
